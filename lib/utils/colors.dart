3560d332ebc856955f08a60839833160c4c3a7e7
Moses Nkrumah <mosessccss@outlook.com>
2020-02-25 19:02:45 +0000

    home setup

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+   Color primaryColor = Color(0xFFD49E15);
+   Color secondaryColor = Color(0xFFF4F1EC);
+   Color textColor = Color(0xFF858489);
+   Color iconColor = Color(0xFFCDCDCD);
+   Color defaultColor = Colors.white;
